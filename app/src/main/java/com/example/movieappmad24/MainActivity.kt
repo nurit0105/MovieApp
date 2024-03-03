9885d8b4452b19383aa4c20ac2bb754c7e9b305b
nurit0105 <laura.davidowicz@stud.fh-campuswien.ac.at>
2024-03-03 20:16:38 +0100

    App Bar Bottom & App Bar Top

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -17,18 +17,22 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,9 +41,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
@@ -55,7 +58,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MovieList(movies = getMovies())
+                    UserInterface()
                 }
             }
         }
@@ -63,8 +66,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MovieList(movies: List<Movie> = getMovies()){
-    LazyColumn {
+fun MovieList(modifier: Modifier, movies: List<Movie> = getMovies()) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(2.dp) // Adjust the spacing as needed
+    ) {
         items(movies) { movie ->
             MovieRow(movie)
         }
@@ -73,14 +79,15 @@ fun MovieList(movies: List<Movie> = getMovies()){
 
 
 @Composable
-fun MovieRow(movie: Movie){
+fun MovieRow(movie: Movie) {
     var showDetails by remember {
         mutableStateOf(false)
     }
 
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(5.dp),
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
         shape = ShapeDefaults.Large,
         elevation = CardDefaults.cardElevation(10.dp)
     ) {
@@ -94,20 +101,22 @@ fun MovieRow(movie: Movie){
                 Image(
                     painter = painterResource(id = R.drawable.movie_image),
                     contentScale = ContentScale.Crop,
-                    contentDescription = "placeholder image")
+                    contentDescription = "placeholder image"
+                )
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(10.dp),
                     contentAlignment = Alignment.TopEnd
-                ){
+                ) {
                     Icon(
                         tint = MaterialTheme.colorScheme.secondary,
                         imageVector = Icons.Default.FavoriteBorder,
-                        contentDescription = "Add to favorites")
+                        contentDescription = "Add to favorites"
+                    )
                 }
             }
-            
+
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -116,18 +125,21 @@ fun MovieRow(movie: Movie){
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(text = movie.title)
-                Icon(modifier = Modifier
-                    .clickable {
-                       showDetails = !showDetails
-                    },
+                Icon(
+                    modifier = Modifier
+                        .clickable {
+                            showDetails = !showDetails
+                        },
                     imageVector =
-                        if (showDetails) Icons.Filled.KeyboardArrowDown
-                        else Icons.Default.KeyboardArrowUp, contentDescription = "show more")
+                    if (showDetails) Icons.Filled.KeyboardArrowDown
+                    else Icons.Default.KeyboardArrowUp, contentDescription = "show more"
+                )
             }
         }
     }
 }
 
+/*
 @Preview
 @Composable
 fun DefaultPreview(){
@@ -135,3 +147,61 @@ fun DefaultPreview(){
        MovieList(movies = getMovies())
     }
 }
+*/
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserInterface() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth().padding(start = 125.dp),
+                title = { Text(text = "Movie App")}
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(start = 20.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            IconButton(
+                                onClick = { /* do something */ }
+                            ) {
+                                Icon(Icons.Default.Home, contentDescription = "Home")
+                            }
+                            Text(text = "Home")
+                        }
+                        Column(
+                            modifier = Modifier.padding(end = 20.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            IconButton(onClick = { /* do something */ }) {
+                                Icon(
+                                    Icons.Default.Star,
+                                    contentDescription = "Watchlist",
+                                )
+                            }
+                            Text(text = "Watchlist")
+                        }
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        MovieList(modifier = Modifier.padding(innerPadding), movies = getMovies())
+    }
+}
+
+
+
