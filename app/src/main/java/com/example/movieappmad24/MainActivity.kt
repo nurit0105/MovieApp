dc0d680917b8312a5e61cc9f7303ba1a3c65e870
nurit0105 <laura.davidowicz@stud.fh-campuswien.ac.at>
2024-03-18 00:33:08 +0100

    Finisehd Exercise 3 Merge Confirmed

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppNavigation(navController)
+                    AppNavigation(navController) // Note to myself: Rendering of the Different Screens work through the AppNavigation
                 }
             }
         }
